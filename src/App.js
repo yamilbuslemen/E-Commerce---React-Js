c8444edf3a00bf9c00fecc3d58cb67c468f260f1
Yamil Buslemen <chamil.rbk@gmail.com>
2023-05-01 00:24:55 -0300

    faltan 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greeting={'Bienvenidos'} />
-
+      <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada ', quantity)}/>
     </div>
   );
 }
---
+++ src/asyncMock.js
@@ -0,0 +1,16 @@
+const products = [
+    {id: '1', name: 'Guantes', price: 12000, category: 'Guantes', stock: 25, description: 'Guantes de boxeo 12/14oz', img: './public/Guantes.webp'},
+    {id: '2', name: 'Vendas', price: 3000, category: 'Vendas', stock: 25, description: 'Vendas protectoras 3mts', img: './public/vendas.webp'},
+    {id: '3', name: 'Bucales', price: 2000, category: 'Bucales', stock: 25, description: 'Protector bucal', img: './public/bucal.webp'},
+    {id: '4', name: 'Proteinas', price: 16000, category: 'Proteinas', stock: 25, description: 'Suplemento nutricional a base de proteinas animales', img: './public/proteinas.webp'},
+    {id: '5', name: 'Batidores', price: 5000, category: 'Batidores', stock: 25, description: 'Vaso batidor para tus suplementos', img: './public/shaker.webp'},
+    {id: '6', name: 'Cinturones', price: 10000, category: 'Cinturones', stock: 25, description: 'Cinturon de fuerza para reforzar tus entrenamientos', img: './public/cinturon.webp'}
+]
+
+export const getProducts = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(products)
+        }, 500)
+    })
+}
