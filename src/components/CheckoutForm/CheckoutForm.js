6d7302174933a1f05bc97212fc9856d1ebd38cd2
Yamil Buslemen <chamil.rbk@gmail.com>
2023-05-22 00:42:06 -0300

    3ra entrega

---
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import './CheckoutForm.css'
+
+
+const CheckoutForm = ({ onConfirm }) => {
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+
+    const handleConfirm = (event) => {
+        event.preventDefault()
+
+        const userData = {
+            name, phone, mail
+        }
+        onConfirm(userData)
+    }
+
+    return (
+        <div className='container'>
+            <form onSubmit={handleConfirm} className='Form'>
+                <label className='Label'>
+                    Nombre
+                    <input
+                    className='Input'
+                    type='text'
+                    value={name}
+                    onChange={({ target }) => setName(target.value)}/>
+                </label>
+                <label className='Label'>
+                    Telefono
+                    <input className='Input'
+                    type='text'
+                    value={phone}
+                    onChange={({ target }) => setPhone(target.value)}/>
+                </label>
+                <label className='Label'>
+                    email
+                    <input className='Input'
+                    type='text'
+                    value={email}
+                    onChange={({ target }) => setEmail(target.value)}/>
+                </label>
+                <div className='Label'>
+                    <button type='submit' className='Button'>Crear Orden</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default CheckoutForm
